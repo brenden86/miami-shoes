4a1baa2e4e48f18daf30006d4b6c338e6454810a
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-06-03 09:24:55 -0500

    fix: refactored styles and js of image-select

--- client/js/modules/product-image-gallery.js
+++ client/js/modules/product-image-gallery.js
@@ -15,12 +15,16 @@ function selectImage(image) {
 
   // put selected image in spotlight
   let selectedImage = image.querySelector('img').cloneNode()
-  selectedImageContainer.innerHTML = ''
-  selectedImageContainer.append(selectedImage)
+  selectedImageContainer.firstElementChild.replaceWith(selectedImage)
 }
 
 thumbnails.forEach(el => {
   el.addEventListener('click', () => {
     selectImage(el)
   })
+})
+
+// prevent dragging thumbnail images
+thumbnails.forEach(thumb => {
+  thumb.querySelector('img').draggable = false
 })
