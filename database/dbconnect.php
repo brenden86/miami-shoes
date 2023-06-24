aa5292f870f8c24dbcf9b4d1e6eb9f54da738d9b
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-06-24 17:54:55 -0500

    populated database

--- database/dbconnect.php
+++ database/dbconnect.php
@@ -1,14 +1,14 @@
 <?php
 
 // load db config constants
-require('../config.php');
+require(__DIR__ . '/../config.php');
 
 $dbname = 'miami_shoes';
 $dsn = "mysql:host=" . DB_HOST . ";dbname=$dbname";
 
 try {
   $db = new PDO($dsn, DB_USER, DB_PASS);
-  echo 'database connected.';
+  echo "database connected.\n";
 } catch (Exception $e) {
   echo $e->getMessage();
 }
