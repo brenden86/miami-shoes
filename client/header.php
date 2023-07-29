451f5ea6d79912710c875a9a7b4e663ef41b3f96
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-07-28 22:55:50 -0500

    fixed my-cart url

--- client/header.php
+++ client/header.php
@@ -17,19 +17,12 @@
       <a href="/client/product-search.php">Sale</a>
     </nav>
     
-    <form id="header-search" class="input-wrapper inline">
-      <input type="text" placeholder="Search"></input>
-      <button type="submit">
-        <i class="bi-search"></i>
-      </button>
-    </form>
-    
   </div>
   
   <!--header right section-->
   <div class="header-section right">
     <div class="header-section-wrapper">
-      <a href="/my-cart" class="icon-link">
+      <a href="/client/my-cart.php" class="icon-link">
         <i class="bi-cart"></i>
         <span>Cart (2)</span>
       </a>
