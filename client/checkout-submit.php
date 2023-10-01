a643c7cdab0d83bf170ad4c2e250bc22860e85a1
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-10-01 16:41:36 -0500

    fixed urls

--- client/checkout-submit.php
+++ client/checkout-submit.php
@@ -15,19 +15,19 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
 // go to next checkout step
 if($step === 1) {
   $_SESSION['checkout_info']['current_step'] = 2;
-  header("location: /client/checkout.php");
+  header("location: /checkout.php");
   exit;
 } elseif($step === 2) {
   $_SESSION['checkout_info']['current_step'] = 3;
-  header("location: /client/checkout.php");
+  header("location: /checkout.php");
   exit;
 } elseif($step === 3) {
   $_SESSION['checkout_info']['current_step'] = 4;
-  header("location: /client/order-confirmation.php");
+  header("location: /order-confirmation.php");
   exit;
 } else {
   $_SESSION['checkout_info']['current_step'] = 1;
-  header("location: /client/checkout.php");
+  header("location: /checkout.php");
   exit;
 }
 
