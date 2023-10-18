50716d33b18f6f729be0f1db1030aae451f8e873
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-10-18 12:48:41 -0500

    setup mobile styles structure

--- client/header.php
+++ client/header.php
@@ -1,82 +1,82 @@
 <header>
   
+  <div class="header-wrapper-main debug">
   <!-- header left section -->
-  <div class="header-section left">
-    <a href="/">
-      <img src="images/logo-placeholder.jpg" class="logo-image" alt="logo" />
-    </a>
-  </div>
-  
-  <!-- header center section -->
-  <div class="header-section center">
-    <!--nav links-->
-    <nav class="header-navigation">
-      <ul class="nav-items">
-        <li>
-          <a href="/product-search.php?mens=true" class="nav-link">Mens</a>
-        </li>
 
-        <li>
-          <a href="/product-search.php?womens=true" class="nav-link">Womens</a>
-        </li>
-
-        <li>
-          <a href="/product-search.php" id="brands-header" class="nav-link">Brands</a>
-
-          <div class="header-hover-menu">
-            <div class="header-hover-menu-wrapper">
-
-              <div class="header-brands-wrapper">
-
-                <a class="brand-card" href="/product-search.php?brand-filter-adidas=adidas">
-                  <img src="./images/logos/Adidas_Logo.png" alt="adidas">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-nike=nike">
-                  <img src="./images/logos/Nike-Logo.png" alt="nike">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-vans=vans">
-                  <img src="./images/logos/vans logo.png" alt="vans">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-converse=converse">
-                  <img src="./images/logos/converse-logo.png" alt="converse">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-dockers=dockers">
-                  <img src="./images/logos/dockers.png" alt="dockers">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-heydude=heydude">
-                  <img src="./images/logos/heydude.png" alt="heydude">
-                </a>
-                <a class="brand-card" href="/product-search.php?brand-filter-newbalance=new+balance">
-                  <img src="./images/logos/new-balance.png" alt="new balance">
-                </a>
-
-              </div>
-            </div>
-          </div>
-        </li>
-
-      </ul>
-    </nav>
-
-  </div>
-  
-  <!--header right section-->
-  <div class="header-section right">
-    <div class="header-section-wrapper">
-      <a href="/my-cart.php" id="header-cart-link" class="icon-link">
-        <i class="bi-cart"></i>
-        Cart<span id="header-cart-count">
-        <?php
-          // show number of cart items
-          if(isset($_COOKIE['cart-items'])) {
-            $cart_cookie = json_decode($_COOKIE['cart-items']);
-            if($cart_cookie && count($cart_cookie) > 0) {
-              echo '(' . count($cart_cookie) . ')';
-            }
-          }
-        ?></span>
+    <div class="header-section left">
+      <a href="/">
+        <img src="images/logo-placeholder.jpg" class="logo-image" alt="logo" />
       </a>
     </div>
+    
+    <!-- header center section -->
+    <div class="header-section center">
+      <!--nav links-->
+      <nav class="header-navigation">
+        <ul class="nav-items">
+          <li>
+            <a href="/product-search.php?mens=true" class="nav-link">Mens</a>
+          </li>
+          <li>
+            <a href="/product-search.php?womens=true" class="nav-link">Womens</a>
+          </li>
+          <li>
+            <a href="/product-search.php" id="brands-header" class="nav-link">Brands</a>
+            <div class="header-hover-menu">
+              <div class="header-hover-menu-wrapper">
+                <div class="header-brands-wrapper">
+                  <a class="brand-card" href="/product-search.php?brand-filter-adidas=adidas">
+                    <img src="./images/logos/Adidas_Logo.png" alt="adidas">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-nike=nike">
+                    <img src="./images/logos/Nike-Logo.png" alt="nike">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-vans=vans">
+                    <img src="./images/logos/vans logo.png" alt="vans">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-converse=converse">
+                    <img src="./images/logos/converse-logo.png" alt="converse">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-dockers=dockers">
+                    <img src="./images/logos/dockers.png" alt="dockers">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-heydude=heydude">
+                    <img src="./images/logos/heydude.png" alt="heydude">
+                  </a>
+                  <a class="brand-card" href="/product-search.php?brand-filter-newbalance=new+balance">
+                    <img src="./images/logos/new-balance.png" alt="new balance">
+                  </a>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    
+    <!--header right section-->
+    <div class="header-section right">
+      <div class="header-section-wrapper">
+        <a href="/my-cart.php" id="header-cart-link" class="icon-link">
+          <i class="bi-cart"></i>
+          Cart<span id="header-cart-count">
+          <?php
+            // show number of cart items
+            if(isset($_COOKIE['cart-items'])) {
+              $cart_cookie = json_decode($_COOKIE['cart-items']);
+              if($cart_cookie && count($cart_cookie) > 0) {
+                echo '(' . count($cart_cookie) . ')';
+              }
+            }
+          ?></span>
+        </a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="header-wrapper-mobile">
+    
   </div>
   
 </header>
