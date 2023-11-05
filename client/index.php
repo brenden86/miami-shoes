ee7643fc65981fd83e08788b254c2f154ede1c11
Brenden Koenigsman <brenden.koenigsman@gmail.com>
2023-11-05 15:43:11 -0600

    Updated feature block text

--- client/index.php
+++ client/index.php
@@ -293,17 +293,17 @@
           <div class="feature-block">
             <i class="bi-truck"></i>
             <h2>Fast n' Free Shipping</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis temporibus id dignissimos iste officia ipsum voluptas voluptatibus.</p>
+            <p>Free shipping available on <em>every</em> pair, expedited shipping is also available if you just can't wait.</p>
           </div>
           <div class="feature-block">
             <i class="bi-clipboard-check"></i>
             <h2>Every Pair Checked for Quality</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis temporibus id dignissimos iste officia ipsum voluptas voluptatibus doloribus consequuntur doloremque dolorem qui pariatur, a nisi alias nihil atque incidunt aliquam.</p>
+            <p>From the laces down to the very sole, every pair is checked for any defects in quality, color, and durability before they leave the store.</p>
           </div>
           <div class="feature-block">
             <i class="bi-award"></i>
             <h2>Award-winning Customer Service</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis temporibus id dignissimos iste officia ipsum voluptas.</p>
+            <p>No long waits or hassle, just simple answers. Get the help you need quickly through our phone and email support team.</p>
           </div>
 
         </div>
