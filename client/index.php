cf4f7d8ed5d014edae7a8006a81373fbbdf47a1a
Brenden Koenigsman <brendenk@BrendenK-MBP-2024.local>
2024-06-01 11:33:42 -0500

    route pages & query params with Router class

--- client/index.php
+++ client/index.php
@@ -1,7 +1,6 @@
 <?php
 
 // AUTOLOAD CLASSES
-
 spl_autoload_register(function ($class) {
   require __DIR__ . '/../' . str_replace('\\', '/', $class) . '.php';
 });
@@ -9,19 +8,22 @@ spl_autoload_register(function ($class) {
 $uri = $_SERVER['REQUEST_URI'];
 
 $page_routes = array(
-  "/" => "/pages/home/home.php",
-  "/404" => "/pages/404/404.php",
-  "/return-policy" => "/pages/return-policy/return-policy.php"
+    "/" => "/pages/home/home.php",
+    "/404" => "/pages/404/404.php",
+    "/return-policy" => "/pages/return-policy/return-policy.php",
+    "/products" => "/pages/products/products.php",
+    "/cart" => "/pages/my-cart/my-cart.php",
+    "/checkout" => "/pages/checkout/checkout.php",
+    "/checkout-submit" => "/pages/checkout/checkout-submit.php",
+    "/faq" => "/pages/faq/faq.php",
+    "/order-submit" => "/pages/checkout/order-submit.php",
+    "/order-confirmation" => "/pages/order-confirmation/order-confirmation.php",
 );
 
 $router = new models\Router();
 $router->register_routes($page_routes);
 
-// debugging
-$uri = '/';
-
-// echo $_SERVER['DOCUMENT_ROOT'];
-
+// serve correct page based on provided path
 try {
   $router->route_request($uri);
 } catch (Error $e) {
